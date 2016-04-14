cbdc549c787618d6171ce4819c6210bbc834e75f
Dany Botelho <danielybotelho@gmail.com>
2016-04-14 01:26:13 -0300

    Add target support files

---
+++ Pods/Target Support Files/Pods-SherlockHolmes/Pods-SherlockHolmes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SherlockHolmes : NSObject
+@end
+@implementation PodsDummy_Pods_SherlockHolmes
+@end
---
+++ Pods/Target Support Files/Pods-SherlockHolmes/Pods-SherlockHolmes-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SherlockHolmesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SherlockHolmesVersionString[];
+
