489254d65d22dded128995d784ee051548258ca3
Dany Botelho <danielybotelho@gmail.com>
2016-04-14 01:30:12 -0300

    Set up API to fetch jsons

--- SherlockHolmes/API/Endpoints.swift
+++ SherlockHolmes/API/Endpoints.swift
@@ -7,7 +7,68 @@
 //
 
 import UIKit
+import Alamofire
 
 class Endpoints: NSObject {
+    
+    //MARK: AGRREEMENTS
+    static func rankingAgreementsByNational(onCompletion: (result: NSDictionary) -> Void) {
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/convenios/convenios_ranking_nacional.json")
+            .responseJSON { response in
+            if let JSON = response.result.value {
+                onCompletion(result: JSON as! NSDictionary)
+            }
+        }
+    }
+    
+    static func rankingAgreementsByRegion(onCompletion: (result: NSDictionary) -> Void) {
+        
 
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/convenios/convenios_regioes.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    onCompletion(result: JSON as! NSDictionary)
+                }
+        }
+    }
+    
+    static func rankingAgreementsByState(onCompletion: (result: NSDictionary) -> Void) {
+        
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/convenios/convenios_estados.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    onCompletion(result: JSON as! NSDictionary)
+                }
+        }
+    }
+    
+    //MARK: TRANSFERS
+    static func rankingTransfersByNational(onCompletion: (result: NSDictionary) -> Void) {
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/transferencias/transferencias_ranking_nacional.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    onCompletion(result: JSON as! NSDictionary)
+                }
+        }
+    }
+    
+    static func rankingTransfersByRegion(onCompletion: (result: NSDictionary) -> Void) {
+        
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/transferencias/transferencias_regioes.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    onCompletion(result: JSON as! NSDictionary)
+                }
+        }
+    }
+    
+    static func rankingTransfersByState(onCompletion: (result: NSDictionary) -> Void) {
+        
+        Alamofire.request(.GET, "http://thiagoguimaraes.com.br/clients/brasilsincero/transferencias/transferencias_estados.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    onCompletion(result: JSON as! NSDictionary)
+                }
+        }
+    }
 }
--- SherlockHolmes/API/ResponseCollectionSerializable.swift
+++ SherlockHolmes/API/ResponseCollectionSerializable.swift
@@ -9,24 +9,32 @@
 import UIKit
 import Alamofire
 
-@objc public protocol ResponseCollectionSerializable {
-    class func collection(#response: NSHTTPURLResponse, representation: AnyObject) -> [Self]
+public protocol ResponseCollectionSerializable {
+    static func collection(response response: NSHTTPURLResponse, representation: AnyObject) -> [Self]
 }
 
 extension Alamofire.Request {
-    public func responseCollection<T: ResponseCollectionSerializable>(completionHandler: (NSURLRequest, NSHTTPURLResponse?, [T]?, NSError?) -> Void) -> Self {
-        let serializer: Serializer = { (request, response, data) in
+    public func responseCollection<T: ResponseCollectionSerializable>(completionHandler: Response<[T], NSError> -> Void) -> Self {
+        let responseSerializer = ResponseSerializer<[T], NSError> { request, response, data, error in
+            guard error == nil else { return .Failure(error!) }
+            
             let JSONSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
-            let (JSON: AnyObject?, serializationError) = JSONSerializer(request, response, data)
-            if response != nil && JSON != nil {
-                return (T.collection(response: response!, representation: JSON!), nil)
-            } else {
-                return (nil, serializationError)
+            let result = JSONSerializer.serializeResponse(request, response, data, error)
+            
+            switch result {
+            case .Success(let value):
+                if let response = response {
+                    return .Success(T.collection(response: response, representation: value))
+                } else {
+                    let failureReason = "Response collection could not be serialized due to nil response"
+                    let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
+                    return .Failure(error)
+                }
+            case .Failure(let error):
+                return .Failure(error)
             }
         }
         
-        return response(serializer: serializer, completionHandler: { (request, response, object, error) in
-            completionHandler(request, response, object as? [T], error)
-        })
+        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
     }
 }
